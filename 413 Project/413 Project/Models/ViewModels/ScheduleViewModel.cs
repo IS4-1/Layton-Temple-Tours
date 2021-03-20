010765ec48df7d645218dba6340b277ef77cb86e
Sangyoon Koo <koosangyoon@gmail.com>
2021-03-20 14:07:46 -0600

    Added a scheduleViewModel

---
+++ 413 Project/413 Project/Models/ViewModels/ScheduleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _413_Project.Models.ViewModels
+{
+    public class ScheduleViewModel
+    {
+        public ScheduleViewModel()
+        {
+        }
+            public IEnumerable<TourModel> Monday { get; set; }
+            public IEnumerable<TourModel> Tuesday { get; set; }
+            public IEnumerable<TourModel> Wednesday { get; set; }
+            public IEnumerable<TourModel> Thursday { get; set; }
+            public IEnumerable<TourModel> Friday { get; set; }
+            public IEnumerable<TourModel> Saturday { get; set; }
+            public IEnumerable<TourModel> Sunday { get; set; }
+    }
+}
